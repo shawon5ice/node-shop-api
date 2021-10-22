d19fd43017faa9e7f72282b39ec55fdee2828427
shawon <shawon5ice@gmail.com>
2021-10-22 19:10:35 +0600

    CORS error handled

--- app.js
+++ app.js
@@ -15,6 +15,18 @@ app.use(bodyParser.urlencoded({ extended:false }))
 //     next(error)
 // })
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', 
+    'Origin, X-Requested-With, Content-Type, Authorization'
+    )
+    if(req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, GET,DELETE,PATCH')
+        return res.status(200).json({})
+    }
+    next()
+})
+
 app.use((error, req, res, next) => {
     res.status(error.status || 500)
     res.json({ 
